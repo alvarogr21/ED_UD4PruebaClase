cad2b6bf76955b0573725f7fd54d736542130d22
Alvaro Gomez Ripoll <alvarogomezripoll@gmail.com>
2023-03-21 09:38:50 +0100

    v2- Poblacion

--- src/prueba/PruebaClase.java
+++ src/prueba/PruebaClase.java
@@ -3,5 +3,6 @@ package prueba;
 public class PruebaClase {
 	public static void main (String [] args) {
 		System.out.println("Alvaro Gomez Ripoll");
+		System.out.println("Picassent - 46220");
 	}
 }
